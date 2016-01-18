bdd6241a1317c7ab584d6e4e53263d72ef521ce2
Tam Dao <daotam4536@gmail.com>
2016-01-19 00:34:05 +0700

    Init and created home page

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular
+  .module('kdq', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ]);
