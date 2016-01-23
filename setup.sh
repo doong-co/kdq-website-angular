f89d548209af606cbb8963184f8affcaa0b61d70
Tam Dao <daotam4536@gmail.com>
2016-01-23 21:33:42 +0700

    Update setup.sh: add command install ruby, rvm and compass

--- setup.sh
+++ setup.sh
@@ -11,7 +11,21 @@ sudo npm install -g live-server
 sudo npm install -g sails
 sudo npm install -g pm2
 
+# Install Ruby and RVM
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+
+curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
+rvm install 2.1.2
+rvm use 2.1.2 --default
+ruby -v
+
+# Install Compass
+gem install compass
+
 export NODE_PATH=/usr/lib/node_modules
 
-(cd ./frontend && npm install)
-(cd ./frontend && bower install)
+(cd ./frontend && sudo npm install)
+(cd ./frontend && sudo bower install --allow-root)
