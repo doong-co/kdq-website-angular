c37e605617327bf50f5e9b1588d32694ec5a8659
Tam Dao <daotam4536@gmail.com>
2016-01-23 20:55:43 +0700

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Init
+sudo apt-get update
+
+# Install Nodejs V5
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo npm install -g grunt-cli
+sudo npm install -g bower
+sudo npm install -g live-server
+sudo npm install -g sails
