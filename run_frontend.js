43f88ecf04c2d681d54727343c95976d2ec4b640
Tam Dao <daotam4536@gmail.com>
2016-01-24 00:39:03 +0700

    Update run server

--- run_frontend.js
+++ run_frontend.js
@@ -1,8 +1,8 @@
 var liveServer = require("live-server");
 
 var params = {
-    port: 8181, // Set the server port. Defaults to 8080.
-    root: "./frontend/app", // Set root directory that's being server. Defaults to cwd.
+    port: 9000, // Set the server port. Defaults to 8080.
+    root: "frontend/dist", // Set root directory that's being server. Defaults to cwd.
     open: false, // When false, it won't load your browser by default.
     file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
     wait: 1000 // Waits for all changes, before reloading. Defaults to 0 sec.
