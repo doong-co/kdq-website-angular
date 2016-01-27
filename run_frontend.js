cf99791dc36c9cd863a56e183c70aceef6faefd4
Tam Dao <daotam4536@gmail.com>
2016-01-28 03:24:14 +0700

    Remove gzip

--- run_frontend.js
+++ run_frontend.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 
-var fileServer = new static.Server('frontend/dist', { gzip: true });
+var fileServer = new static.Server('frontend/dist', { cache: 7200 });
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
