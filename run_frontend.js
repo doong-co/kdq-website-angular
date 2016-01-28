65f769d5cc76361f8718d6a4e56ed90a490477a3
Tam Dao <daotam4536@gmail.com>
2016-01-28 12:14:27 +0700

    Use expressjs

--- run_frontend.js
+++ run_frontend.js
@@ -1,18 +1,12 @@
-var static = require('node-static');
+var express = require('express');
+var compression = require('compression');
 
-var fileServer = new static.Server('frontend/dist', { cache: 7200, gzip: true });
+var app = express();
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response, function (err, result) {
-            if (err && (err.status === 404)) {
-              fileServer.serveFile('/404.html', 404, {}, request, response);
-            } else if (err) { // There was an error serving the file
-                console.error("Error serving " + request.url + " - " + err.message);
+var oneDay = 86400000;
 
-                response.writeHead(err.status, err.headers);
-                response.end();
-            }
-        });
-    }).resume();
-}).listen(9000);
+app.use(compression());
+
+app.use(express.static('./frontend/dist', { maxAge: oneDay }));
+
+app.listen(9000);
