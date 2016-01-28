5989babd9f691d8ecbe1b47692acf9c2ec0491b4
Tam Dao <daotam4536@gmail.com>
2016-01-28 11:19:01 +0700

    Add canvas bg into anather section

--- frontend/app/scripts/widgets/kdq_trianglify.directive.js
+++ frontend/app/scripts/widgets/kdq_trianglify.directive.js
@@ -8,15 +8,23 @@
   function kdqTrianglifyDirective($window, $log) {
     return {
       restrict: 'A',
-      link: function($scope, $element) {
+      link: function($scope, $element, $attr) {
         if (!_.isFunction($window.Trianglify)) {
           return $log.error('Trianglify is not function');
         }
-
-        var pattern = Trianglify();
+        // var colors = ['Greys', 'Blues', 'RdYlBu', 'PuOr', 'YlOrRd', 'PuBu', 'Purples'];
+        var pattern = Trianglify({
+          width: $element.width(),
+          height: $element.height(),
+          x_colors: $attr.color || 'random'
+        });
         pattern.canvas($element[0]);
       }
     }
+  }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
 })();
