eb08e99eff8caca1df39fb2e56d5200b513f63f2
Tam Dao <daotam4536@gmail.com>
2016-02-01 14:40:46 +0700

    Apply rerender

--- server_dev.js
+++ server_dev.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var compression = require('compression');
 var livereload = require('connect-livereload');
+var consolidate = require('consolidate');
 
 var app = express();
 
@@ -10,13 +11,44 @@ var PORT = 3000;
 app.use(compression());
 app.use(livereload());
 
+app.set('view engine', 'html');
+app.set('views', __dirname + '/frontend/app');
+app.engine('html', consolidate.mustache);
+
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: "SOLID offshore front-end development",
+    subTitle: "Our model heavily leverages long-term partnerships. You can count on us for on-time, on-budget delivery, and awesome products.",
+    canDisplayBtn: true
+  });
+});
+
 app.use(express.static(__dirname + '/frontend', { maxAge: oneDay }));
 app.use(express.static(__dirname + '/frontend/app', { maxAge: oneDay }));
 app.use(express.static(__dirname + '/frontend/.tmp', { maxAge: oneDay }));
-app.use(express.static(__dirname + '/frontend/bower_components', { maxAge: oneDay }));
+
+app.get('/our-services', function(req, res, next) {
+  res.render('index', {
+    title: "Services",
+    subTitle: "We have our developers working and communicating directly with our clients’ engineering team. We use Agile processes and professional tools such as Jira and Slack to keep track of development progress and better collaborate with our clients."
+  });
+});
+
+app.get('/about-us', function(req, res, next) {
+  res.render('index', {
+    title: "About us"
+  });
+});
+
+app.get('/404', function(req, res, next) {
+  res.render('index', {
+    title: "ERROR 404",
+    subTitle: "It's looking like you may have taken a wrong turn.<br>Don't worry... It happens to the best of us."
+  });
+});
 
 app.all('/*', function(req, res, next) {
-  res.sendFile('index.html', { root: __dirname + '/frontend/app' });
+  res.redirect('/404');
 });
 
 app.listen(PORT, function() {
--- server_prod.js
+++ server_prod.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var compression = require('compression');
+var consolidate = require('consolidate');
 
 var app = express();
 
@@ -8,10 +9,42 @@ var PORT = 9000;
 
 app.use(compression());
 
+app.set('view engine', 'html');
+app.set('views', __dirname + '/frontend/dist');
+app.engine('html', consolidate.mustache);
+
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: "SOLID offshore front-end development",
+    subTitle: "Our model heavily leverages long-term partnerships. You can count on us for on-time, on-budget delivery, and awesome products.",
+    canDisplayBtn: true
+  });
+});
+
 app.use(express.static(__dirname + '/frontend/dist', { maxAge: oneDay }));
 
+app.get('/our-services', function(req, res, next) {
+  res.render('index', {
+    title: "Services",
+    subTitle: "We have our developers working and communicating directly with our clients’ engineering team. We use Agile processes and professional tools such as Jira and Slack to keep track of development progress and better collaborate with our clients."
+  });
+});
+
+app.get('/about-us', function(req, res, next) {
+  res.render('index', {
+    title: "About us"
+  });
+});
+
+app.get('/404', function(req, res, next) {
+  res.render('index', {
+    title: "ERROR 404",
+    subTitle: "It's looking like you may have taken a wrong turn.<br>Don't worry... It happens to the best of us."
+  });
+});
+
 app.all('/*', function(req, res, next) {
-  res.sendFile('index.html', { root: __dirname + '/frontend/dist' });
+  res.redirect('/404');
 });
 
 app.listen(PORT, function() {
