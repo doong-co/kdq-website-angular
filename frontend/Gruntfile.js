482ee59306678355403f1e073ea2b9efb678623c
Tam Dao <daotam4536@gmail.com>
2016-02-04 20:16:52 +0700

    Update build script to make reduce css

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -606,7 +606,7 @@ module.exports = function(grunt) {
       dist: {
         options: {
           stylesheets: ['../.tmp/styles/kdq.css'],
-          ignore: [/dropdown-menu/,/\.collapsing/,/\.collapse/, /.navbar-nav/, /.nav-white/, /.map/]
+          ignore: [/\.dropdown-menu/,/\.collapsing/,/\.collapse/, /\.nav-link \+ \.nav-link/, /\.nav-white/, /\.map/]
         },
         files: {
           '.tmp/styles/kdq.css': [
